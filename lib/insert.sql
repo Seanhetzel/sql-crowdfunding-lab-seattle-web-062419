64ae85b1e2ea5e1aca821dc38b333ff5f85c43d9
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-08 15:59:22 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -18,5 +18,47 @@ INSERT INTO users (name, age) VALUES ("Jackoline", "26");
 INSERT INTO users (name, age) VALUES ("Jackoline", "26");
 INSERT INTO users (name, age) VALUES ("Jackoline", "26");
 INSERT INTO users (name, age) VALUES ("Jackoline", "26");
-INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("Coolest Coffee Machine", "Tech", 50,000,000, "6/28/2019", "12/31/2019");
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(2, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(3, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(4, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(5, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(6, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(7, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(8, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(9, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019"),
+(10, "Coolest Coffee Machine", "Tech", 50000000, "6/28/2019", "12/31/2019");
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10000, 5, 8),
+(2, 10000, 5, 8),
+(3, 10000, 5, 8),
+(4, 10000, 5, 8),
+(5, 10000, 5, 8),
+(6, 10000, 5, 8),
+(7, 10000, 5, 8),
+(8, 10000, 5, 8),
+(9, 10000, 5, 8),
+(10, 10000, 5, 8),
+(11, 10000, 5, 8),
+(12, 10000, 5, 8),
+(13, 10000, 5, 8),
+(14, 10000, 5, 8),
+(15, 10000, 5, 8),
+(16, 10000, 5, 8),
+(17, 10000, 5, 8),
+(18, 10000, 5, 8),
+(19, 10000, 5, 8),
+(20, 10000, 5, 8),
+(21, 10000, 5, 8),
+(22, 10000, 5, 8),
+(23, 10000, 5, 8),
+(24, 10000, 5, 8),
+(25, 10000, 5, 8),
+(26, 10000, 5, 8),
+(27, 10000, 5, 8),
+(28, 10000, 5, 8),
+(29, 10000, 5, 8),
+(30, 10000, 5, 8);
